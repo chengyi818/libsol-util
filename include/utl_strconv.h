4bd76fde078b3b950cd5d3a0cb1cde099961a643
sphantix <sphantix@gmail.com>
2015-09-30 15:37:31 +0800

    Develop | [STRING] Add string utilities

--- include/utl_strconv.h
+++ include/utl_strconv.h
@@ -15,7 +15,16 @@
 
 #include "utl.h"
 
+void utlStr_strToLower(char *string);
+void utlStr_strToUpper(char *string);
+SINT32 utlStr_strcmp(const char *s1, const char *s2);
+SINT32 utlStr_strcasecmp(const char *s1, const char *s2);
+SINT32 utlStr_strncmp(const char *s1, const char *s2, SINT32 n);
+SINT32 utlStr_strncasecmp(const char *s1, const char *s2, SINT32 n);
+char *utlStr_strstr(const char *s1, const char *s2);
+char *utlStr_strncpy(char *dest, const char *src, SINT32 dlen);
 char *utlStr_strcpy(char *dest, const char *src);
+char *utlStr_strcat(char *dest, const char *src);
 SINT32 utlStr_strlen(const char *src);
 char *utlStr_strdup(const char *src);
 char *utlStr_getBaseName(char *path);
--- utl_strconv.c
+++ utl_strconv.c
@@ -16,8 +16,137 @@
 #include <sys/stat.h>  /* this really should be in oal_strconv.c */
 #include <arpa/inet.h> /* for inet_aton */
 #include <sys/time.h> /* for inet_aton */
-
 #include "utl_strconv.h"
+#include "utl_logging.h"
+
+void utlStr_strToLower(char *string)
+{
+   char *ptr = string;
+   for (ptr = string; *ptr; ptr++)
+   {
+       *ptr = tolower(*ptr);
+   }
+}
+
+void utlStr_strToUpper(char *string)
+{
+   char *ptr = string;
+   for (ptr = string; *ptr; ptr++)
+   {
+       *ptr = toupper(*ptr);
+   }
+}
+
+SINT32 utlStr_strcmp(const char *s1, const char *s2)
+{
+   char emptyStr = '\0';
+   char *str1 = (char *) s1;
+   char *str2 = (char *) s2;
+
+   if (str1 == NULL)
+   {
+      str1 = &emptyStr;
+   }
+   if (str2 == NULL)
+   {
+      str2 = &emptyStr;
+   }
+
+   return strcmp(str1, str2);
+}
+
+SINT32 utlStr_strcasecmp(const char *s1, const char *s2)
+{
+   char emptyStr = '\0';
+   char *str1 = (char *) s1;
+   char *str2 = (char *) s2;
+
+   if (str1 == NULL)
+   {
+      str1 = &emptyStr;
+   }
+   if (str2 == NULL)
+   {
+      str2 = &emptyStr;
+   }
+
+   return strcasecmp(str1, str2);
+}
+
+SINT32 utlStr_strncmp(const char *s1, const char *s2, SINT32 n)
+{
+   char emptyStr = '\0';
+   char *str1 = (char *) s1;
+   char *str2 = (char *) s2;
+
+   if (str1 == NULL)
+   {
+      str1 = &emptyStr;
+   }
+   if (str2 == NULL)
+   {
+      str2 = &emptyStr;
+   }
+
+   return strncmp(str1, str2, n);
+}
+
+SINT32 utlStr_strncasecmp(const char *s1, const char *s2, SINT32 n) 
+{
+   char emptyStr = '\0';
+   char *str1 = (char *) s1;
+   char *str2 = (char *) s2;
+
+   if (str1 == NULL)
+   {
+      str1 = &emptyStr;
+   }
+   if (str2 == NULL)
+   {
+      str2 = &emptyStr;
+   }
+
+   return strncasecmp(str1, str2, n);
+}
+
+char *utlStr_strstr(const char *s1, const char *s2) 
+{
+   char emptyStr = '\0';
+   char *str1 = (char *)s1;
+   char *str2 = (char *)s2;
+
+   if (str1 == NULL)
+   {
+      str1 = &emptyStr;
+   }
+   if (str2 == NULL)
+   {
+      str2 = &emptyStr;
+   }
+
+   return strstr(str1, str2);
+}
+
+char *utlStr_strncpy(char *dest, const char *src, SINT32 dlen)
+{
+
+   if((src == NULL) || (dest == NULL))
+   {
+      utlLog_error("null pointer reference src =%u ,dest =%u", src, dest);
+   }	
+
+   if( strlen(src)+1 > dlen )
+   {
+      utlLog_notice("truncating:src string length > dest buffer");
+      strncpy(dest,src,dlen-1);
+      dest[dlen-1] ='\0';
+   }
+   else
+   {
+      strcpy(dest,src);
+   }
+   return dest;
+}
 
 char *utlStr_strcpy(char *dest, const char *src)
 {
@@ -35,6 +164,24 @@ char *utlStr_strcpy(char *dest, const char *src)
    }
 
    return strcpy(str1, str2);   
+}
+
+char *utlStr_strcat(char *dest, const char *src)
+{
+   char emptyStr = '\0';
+   char *str1 = (char *)dest;
+   char *str2 = (char *)src;
+
+   if (str1 == NULL)
+   {
+      str1 = &emptyStr;
+   }
+   if (str2 == NULL)
+   {
+      str2 = &emptyStr;
+   }
+
+   return strcat(str1, str2);      
 }
 
 SINT32 utlStr_strlen(const char *src)
